c339cf97d48405a29dac3a486373202c6e83ab1d
Aidan Hurley-Kalici <57423704+Aidanjhk@users.noreply.github.com>
2020-01-05 19:06:16 -0500

    t

--- run.sh
+++ run.sh
@@ -6,5 +6,3 @@ git add .
 git commit -m "Updated Coordinates file"
 git remote add Object_Data - Sheet1.csv
 git push origin master
-start chrome https://sheets.googleapis.com/v4/spreadsheets/d/1KGNQTs7E8qLLWi2FCfdbDisnIiwao2IKRWMIksAI2p0/values/Sheet1!A2:F811:clear
-
